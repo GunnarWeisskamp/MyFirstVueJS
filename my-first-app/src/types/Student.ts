7bfd7cfe01d28f14c2269da694628c5ce32cf26d
gweisskamp <gullarver@hotmail.com>
2021-05-10 05:44:13 +1000

    first

---
+++ my-first-app/src/types/Student.ts
@@ -0,0 +1,6 @@
+interface Student {
+    fullName: string;
+    age: number;
+  }
+  
+  export default Student;
---
+++ my-first-app/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <HelloWorld></HelloWorld><br>
+    <ShowStudent></ShowStudent>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.ts';
+import ShowStudent from '@/components/ShowStudent.ts';
+
+@Component({
+  components: {
+    HelloWorld,
+    ShowStudent,
+  },
+})
+export default class Home extends Vue {}
+</script>
